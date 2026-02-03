b0d234e2dfc99dd71bdac3c09c32571a52527d2f
MrBanana17 <jjramirez1776@gmail.com>
2026-02-03 14:40:33 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://BananaCo.github.io',
+  base: '/BananaCo.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
